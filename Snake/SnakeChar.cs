ed84d4ae23a68583c144d60843c618a769fe98c2
Balen Dezai <Balen1996@hotmail.com>
2017-10-06 13:18:40 +0200

    Added Properties, Fields and Enums to SnakeChar Class

---
+++ Snake/SnakeChar.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class SnakeChar
+    {
+
+        private int _lenght;
+        public int Lenght
+        {
+            get { return _lenght; }
+            set { _lenght = value; }
+        }
+
+        enum Color
+        {
+            Red, 
+            Green,
+            Black,
+            Yellow,
+            Blue
+
+        }
+
+        enum Status
+        {
+            Dead,
+            Alive
+        }
+
+
+
+        /*void Test(Color color)
+        {
+            switch (color)
+            {
+                case Color.Red:
+                    break;
+                case Color.Green:
+                    break;
+                case Color.Black:
+                    break;
+                case Color.Yellow:
+                    break;
+                case Color.Blue:
+                    break;
+                default:
+                    break;
+            }
+        }*/
+
+    }
+}
