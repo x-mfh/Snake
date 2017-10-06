c3f70f9c000b97943eb4c5c8a053d961d5d7d2fd
Mikkel Hammer <Mikkel_Fhammer@hotmail.com>
2017-10-06 13:33:25 +0200

    Added Player and Game classes

---
+++ Snake/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Game
+    {
+        public enum Difficulty
+        {
+            Easy,
+            Hard
+        }
+
+        public void CreateBorder(Difficulty diff)
+        {
+
+        }
+
+        public void FoodGen() // Take x and y ?
+        {
+
+        }
+
+    }
+}
