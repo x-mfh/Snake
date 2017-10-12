91dfd4f514275b60135786f055aad35cac981568
Balen Dezai <balen1996@hotmail.com>
2017-10-12 14:17:45 +0200

    Went back to WPF now that we can use WPF again

--- Snake/Properties/Resources.Designer.cs
+++ Snake/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SnakeV2.Properties
+namespace Snake.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace SnakeV2.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SnakeV2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Snake.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Snake/Properties/Settings.Designer.cs
+++ Snake/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SnakeV2.Properties
+namespace Snake.Properties
 {
 
 
